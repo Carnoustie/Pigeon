0674fbf0d80bff391c36fccab3f2d81c6245a97b
Carnoustie <oscar.spolander@gmail.com>
2026-03-22 19:50:43 +0100

    EventWriterSimulator

--- src/EventWriterSimulator.cpp
+++ src/EventWriterSimulator.cpp
@@ -3,7 +3,10 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <ctime>
+#include <locale>
 #include <random>
+#include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -32,7 +35,7 @@ std::mt19937 randomEngine(19);
 std::normal_distribution<double> gaussian(0,1);
 std::uniform_int_distribution<> uniformSampler(500, 1500);
 
-void playTradingInterval(int recvSockFD, addrinfo recv, int durationInSeconds){
+void playTradingInterval(int recvSockFD, addrinfo* recv, int durationInSeconds){
 
 	int duration = durationInSeconds*1000000;
 
@@ -45,37 +48,47 @@ void playTradingInterval(int recvSockFD, addrinfo recv, int durationInSeconds){
 		"NVIDIA",
 		999,
 		197.92
-	}
+	};
 
 	char logString[4096];
-	int bytesWritten = fprintf(logString, "\n\nAt %s, %s purchased %d units of %s from %s at price %f", current, tradingEvent.buyer, tradingEvent.numUnits, tradingEvent.ticker, tradingEvent.seller, tradingEvent.price)
+	char timeString[64];
+	uint64_t t = time(nullptr);
+	std::time_t tt = (std::time_t) t;
+	int bytesWritten = strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", std::localtime(&tt));
+	bytesWritten = sprintf(logString, "\n\nAt %s, %s purchased %d units of %s from %s at price %f", timeString, tradingEvent.buyer, tradingEvent.numUnits, tradingEvent.ticker, tradingEvent.seller, tradingEvent.price);
 
-	FTE fullEvent = {
-		start,
-		logString,
-		FTP
-	};
+
+
+	FTE fullEvent;
+	fullEvent.timestamp = t;
+	memcpy(fullEvent.logMessage, logString, sizeof(logString));
+	fullEvent.payload = tradingEvent;
 
 	int bytesSent;
 
 	FTE eventBuffer[1];
-	eventBuffer[0] = FTE;
+	eventBuffer[0] = fullEvent;
 
-	while(std::chrono::duration_cast<std::chrono::milliseconds>(current-start).now()<duration){
+	while(std::chrono::duration_cast<std::chrono::milliseconds>(current-start).count()<duration){
 		std::this_thread::sleep_for(std::chrono::seconds(1));
-		bytesWritten = fprintf(logString, "\n\nAt %s, %s purchased %d units of %s from %s at price %f", current, tradingEvent.buyer, tradingEvent.numUnits, tradingEvent.ticker, tradingEvent.seller, tradingEvent.price);
+		t = time(nullptr);
+		tt = (std::time_t) t;
+		bytesWritten = strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", std::localtime(&tt));
+		bytesWritten = sprintf(logString, "\n\nAt %s, %s purchased %d units of %s from %s at price %f", timeString, tradingEvent.buyer, tradingEvent.numUnits, tradingEvent.ticker, tradingEvent.seller, tradingEvent.price);
 		tradingEvent.price = tradingEvent.price + gaussian(randomEngine);
 		tradingEvent.numUnits = uniformSampler(randomEngine);
-		fullEvent.payload = FTP;
-		eventBuffer[0] = FTE;
-		bytesSent = recv(recvSockFD, eventBuffer, sizeof(eventBuffer), 0);
+		fullEvent.timestamp = t;
+		memcpy(fullEvent.logMessage, logString, sizeof(logString));
+		fullEvent.payload = tradingEvent;
+		eventBuffer[0] = fullEvent;
+		bytesSent = sendto(recvSockFD, eventBuffer, sizeof(eventBuffer), 0, recv->ai_addr, recv->ai_addrlen);
 	}
 }
 
 int main(int argc, char* argv[]){
 
 	struct addrinfo hints;
-	struct addrinfo result;
+	struct addrinfo* result;
 
 	memset(&hints, 0, sizeof(hints));
 
@@ -92,4 +105,5 @@ int main(int argc, char* argv[]){
 	if(connect(sockFD, result->ai_addr, result->ai_addrlen)!=0){
 		printf("Failed connecting due to %s", strerror(errno));
 	}
+	playTradingInterval(sockFD, result, 10);
 }
