4db137a488d19cb9910b037064e4fbc3d7a45c75
Carnoustie <oscar.spolander@gmail.com>
2026-03-24 09:02:44 +0100

    EventBroker design outline

--- src/EventBroker.hpp
+++ src/EventBroker.hpp
@@ -17,8 +17,8 @@ class EventBroker{
 		int maxEventReaderConnections;
 		ServerTCPsocket evtWrtrSock;
 		ServerTCPsocket evtRdrSock;
-		const char* evtWrtrPort = "8080";
-		const char* evtRdrPort = "8081";
+		const char* evtWrtrsPort = "8080";
+		const char* evtRdrsPort = "8081";
 		conn* wrtrConns;
 		conn* rdrConns;
 	
@@ -26,8 +26,8 @@ class EventBroker{
 		EventBroker(int maxWriters, int maxReaders){
 			maxEventWriterConnections = maxWriters;
 			maxEventReaderConnections = maxReaders;
-			evtWrtrSock = ServerTCPsocket(evtWrtrPort, maxWriters);
-			evtRdrSock = ServerTCPsocket(evtRdrPort, maxReaders);
+			evtWrtrSock = ServerTCPsocket(evtWrtrsPort, maxWriters);
+			evtRdrSock = ServerTCPsocket(evtRdrsPort, maxReaders);
 			wrtrConns = new conn[maxWriters];
 			rdrConns = new conn[maxReaders];
 		}
